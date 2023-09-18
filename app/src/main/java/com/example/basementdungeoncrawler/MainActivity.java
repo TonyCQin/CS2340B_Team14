c44d91a3fbbcdb0052f0fad11f696036b5b28031
Tony Qin <qin.tony123@gmail.com>
2023-09-18 14:54:12 -0400

    add MainActivity

---
+++ app/src/main/java/com/example/basementdungeoncrawler/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.basementdungeoncrawler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
