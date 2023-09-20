7635750d07786efb2dd5ae0b460c12a2c6371cf6
Tony Qin <qin.tony123@gmail.com>
2023-09-20 01:45:27 -0400

    saving progress for now. Implement some sort of configuration screen with char1, char2, char 3 buttons that change text indicating they were clicked on. Created XML file that runs on emulation of main activity

--- app/src/main/java/com/example/basementdungeoncrawler/MainActivity.java
+++ app/src/main/java/com/example/basementdungeoncrawler/MainActivity.java
@@ -1,12 +1,52 @@
 package com.example.basementdungeoncrawler;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
+    private EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button char1 = findViewById(R.id.char1);
+        Button char2 = findViewById(R.id.char2);
+        Button char3 = findViewById(R.id.char3);
+        TextView charText = findViewById(R.id.charTextView);
+
+        char1.setOnClickListener(v -> {
+            charText.setText("Character 1 selected");
+        });
+
+        char2.setOnClickListener(v -> {
+            charText.setText("Character 2 selected");
+        });
+
+        char3.setOnClickListener(v -> {
+            charText.setText("Character 3 selected");
+        });
+
+        RadioGroup difficulties = findViewById(R.id.difficultyRadioGroup);
+        difficulties.setOnClickListener(v -> {
+            double difficulty = 1;
+        });
+    }
+
+    public void onSubmitClick(View view) {
+        String inputText = editText.getText().toString().trim();
+
+        if (!inputText.isEmpty()) {
+            Toast.makeText(this, "You entered: " + inputText, Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Please enter a name", Toast.LENGTH_SHORT).show();
+        }
     }
 }
