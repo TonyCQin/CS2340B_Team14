c00ca0f2141b6f9a46fa62fff0020b42d1494f17
Jeffrey Ding <jzbding@gmail.com>
2023-09-21 22:39:40 -0400

    Add the EndScreen

---
+++ app/src/main/java/com/example/basementdungeoncrawler/EndScreen.java
@@ -0,0 +1,22 @@
+package com.example.basementdungeoncrawler;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EndScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //sets the view to the UI of the end screen
+        setContentView(R.layout.end_screen);
+        //sets up the restart button
+        Button toStartScreen = findViewById(R.id.toStartScreen);
+        toStartScreen.setOnClickListener(v -> {
+            Intent restart = new Intent(EndScreen.this, StartScreen.class);
+            startActivity(restart);
+        });
+    }
+}
