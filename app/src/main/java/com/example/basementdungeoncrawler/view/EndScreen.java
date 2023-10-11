c1a9f88e36e47f47f8cde25fc3699ae47516dc59
demilei221 <demilei221@gmail.com>
2023-10-11 01:15:49 -0400

    leaderboard view setup complete, need adjustment

--- app/src/main/java/com/example/basementdungeoncrawler/view/EndScreen.java
+++ app/src/main/java/com/example/basementdungeoncrawler/view/EndScreen.java
@@ -1,9 +1,12 @@
 package com.example.basementdungeoncrawler.view;
 
+import com.example.basementdungeoncrawler.Model.ScoresList;
 import com.example.basementdungeoncrawler.R;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -19,5 +22,18 @@ public class EndScreen extends AppCompatActivity {
             Intent restart = new Intent(EndScreen.this, MainActivity.class);
             startActivity(restart);
         });
+        ListView leaderBoard = findViewById(R.id.leaderBoard);
+        // Define the data you want to display in the ListView
+        String[] data = {"player 1", "player 2", "player 3", "player 4", "player 5"};
+        ScoresList scoreslist = ScoresList.getList();
+//        TODO: scoreslist.getTopFive.list
+//        pass that into the adapter
+
+        // Create an ArrayAdapter to bind the data to the ListView
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, data);
+
+        // Set the adapter to the ListView
+        leaderBoard.setAdapter(adapter);
     }
 }
+
