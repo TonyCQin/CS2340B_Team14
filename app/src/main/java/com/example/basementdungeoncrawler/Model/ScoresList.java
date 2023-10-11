c86153d41483612424884ef9fad08405f68931af
Tony Qin <qin.tony123@gmail.com>
2023-10-11 14:08:12 -0400

    create an object with score used in passing information to the leaderboard

--- app/src/main/java/com/example/basementdungeoncrawler/Model/ScoresList.java
+++ app/src/main/java/com/example/basementdungeoncrawler/Model/ScoresList.java
@@ -1,14 +1,20 @@
 package com.example.basementdungeoncrawler.Model;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Collections;
+import com.example.basementdungeoncrawler.Model.Score;
+//time imports
+import org.threeten.bp.zone.TzdbZoneRulesProvider;
+import org.threeten.bp.zone.ZoneRulesProvider;
+import org.threeten.bp.ZonedDateTime;
 
 public class ScoresList {
     private volatile static ScoresList list;
     //all recorded scores
-    private ArrayList<Integer> scores;
+    private ArrayList<Score> scores;
     //relevant scores
-    private ArrayList<Integer> topFive;
+    private ArrayList<Score> topFive;
 
     private ScoresList() {
         scores = new ArrayList<>();
@@ -28,14 +34,15 @@ public class ScoresList {
     }
 
     //adds new score to list to display on leaderboard and enforces descending order
-    public synchronized void addScore(int newScore) {
-        scores.add(newScore);
+    public synchronized void addScore(String username, int newScore) {
+        Score score = new Score(username, newScore);
+        scores.add(score);
         Collections.sort(scores, Collections.reverseOrder());
         updateScores();
     }
 
     //retrieves relevant list of scores
-    public synchronized ArrayList<Integer> getScores() {
+    public synchronized ArrayList<Score> getScores() {
         return topFive;
     }
 
@@ -47,4 +54,17 @@ public class ScoresList {
         }
     }
 
+    public static <T extends Comparable<T>> void bubbleSort(List<T> list) {
+        int n = list.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (list.get(j).compareTo(list.get(j + 1)) > 0) {
+                    // Swap elements if they are in the wrong order
+                    T temp = list.get(j);
+                    list.set(j, list.get(j + 1));
+                    list.set(j + 1, temp);
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/basementdungeoncrawler/viewModel/EndScreenViewModel.java
+++ app/src/main/java/com/example/basementdungeoncrawler/viewModel/EndScreenViewModel.java
@@ -1,12 +1,14 @@
 package com.example.basementdungeoncrawler.viewModel;
 
 import com.example.basementdungeoncrawler.Model.ScoresList;
+import com.example.basementdungeoncrawler.Model.Score;
 
 import java.util.ArrayList;
 
 public class EndScreenViewModel {
         ScoresList scoreslist = ScoresList.getList();
-        ArrayList<Integer> top5 = scoreslist.getScores();
+        ArrayList<Score> top5 = scoreslist.getScores();
+
 
         //TODO: parse data here so endscreen can directcly generate it
 }
