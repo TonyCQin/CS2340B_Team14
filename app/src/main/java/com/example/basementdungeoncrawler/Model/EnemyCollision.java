865f545da571881ade347994c87620f1490a312f
mngo39 <mngo39@gatech.edu>
2023-11-12 22:57:24 -0500

    worked on enemy collisions

--- app/src/main/java/com/example/basementdungeoncrawler/Model/EnemyCollision.java
+++ app/src/main/java/com/example/basementdungeoncrawler/Model/EnemyCollision.java
@@ -19,7 +19,7 @@ public class EnemyCollision implements PlayerSubscriber, EnemySubscriber {
 
     private TileMap tileMap;
     private final ArrayList<Integer> tileWallIds = new ArrayList<>(Arrays.asList(0, 1, 2, 3,
-        25, 26, 27, 28, 50, 51, 52, 53, 75, 76, 77, 78, 100, 101, 102, 103, 104, 125, 126, 127, 128,
+        25, 26, 27, 28, 50, 51, 52, 53, 75, 76, 77, 78, 100, 101, 102, 103, 104, 125, 126,
         129, 130, 150, 151, 152, 153, 154, 350, 351, 352, 353, 354, 355, 356, 357, 358, 359, 375,
         376, 377, 378, 379, 380, 381, 382, 383, 384, 400, 401, 402, 403, 404, 405, 406, 407, 408,
         451, 454, 457, 486, 185, 186, 210, 211));
@@ -30,6 +30,8 @@ public class EnemyCollision implements PlayerSubscriber, EnemySubscriber {
     private double playerPosX;
     private double playerPosY;
     private double playerR;
+
+    private int speed;
 
     public EnemyCollision(TileMap t) {
         tileMap = t;
@@ -64,12 +66,12 @@ public class EnemyCollision implements PlayerSubscriber, EnemySubscriber {
     }
 
     @Override
-    public void updateEnemyPosition(double positionX, double positionY, double radius) {
+    public void updateEnemyPosition(double positionX, double positionY, double radius, int speed) {
         this.positionX = positionX;
         this.positionY = positionY;
         this.radius = radius;
         checkCollisionWithPlayer();
-        checkCollisionWithWall();
+        checkCollisionWithWall(positionX, positionY);
     }
 
     private void checkCollisionWithPlayer() {
@@ -84,11 +86,16 @@ public class EnemyCollision implements PlayerSubscriber, EnemySubscriber {
         }
     }
 
-    private void checkCollisionWithWall() {
-        right = tileWallIds.contains(getTileId(tileMap, this.positionX + radius, this.positionY));
-        left = tileWallIds.contains(getTileId(tileMap, this.positionX - radius, this.positionY));
-        bottom = tileWallIds.contains(getTileId(tileMap, this.positionX, this.positionY + radius));
-        up = tileWallIds.contains(getTileId(tileMap, this.positionX, this.positionY - radius));
+    private void checkCollisionWithWall(double posX, double posY) {
+        right = tileWallIds.contains(getTileId(tileMap, this.positionX + radius,
+                this.positionY)) || tileWallIds.contains(getTileId(tileMap,
+                this.positionX + speed, this.positionY)) ;
+        left = tileWallIds.contains(getTileId(tileMap, this.positionX - radius, this.positionY)) ||
+                tileWallIds.contains(getTileId(tileMap, this.positionX - speed, this.positionY));
+        bottom = tileWallIds.contains(getTileId(tileMap, this.positionX, this.positionY + radius)) ||
+                tileWallIds.contains(getTileId(tileMap, this.positionX, this.positionY + speed));
+        up = tileWallIds.contains(getTileId(tileMap, this.positionX, this.positionY - radius)) ||
+                tileWallIds.contains(getTileId(tileMap, this.positionX, this.positionY - speed));
     }
 
     private int getTileId(TileMap tilemap, double positionX, double positionY) {
--- app/src/main/java/com/example/basementdungeoncrawler/Model/Shadow.java
+++ app/src/main/java/com/example/basementdungeoncrawler/Model/Shadow.java
@@ -25,7 +25,7 @@ public class Shadow extends Enemy {
 
     public Shadow(Context context, double positionX, double positionY, int hp, int damage,
                   int radius, int speed) {
-        super(context, positionX, positionY, hp, 100, speed, new Paint());
+        super(context, positionX, positionY, hp, radius, speed, new Paint());
         super.setDamage(damage);
 
         paint = new Paint();
