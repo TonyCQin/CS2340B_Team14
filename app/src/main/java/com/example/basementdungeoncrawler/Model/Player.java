dd7e68ec06bfaf2f5aa65cfbe7f1a14fa6012c20
mngo39 <mngo39@gatech.edu>
2023-11-28 21:52:31 -0500

    javadocs to Player

--- app/src/main/java/com/example/basementdungeoncrawler/Model/Player.java
+++ app/src/main/java/com/example/basementdungeoncrawler/Model/Player.java
@@ -20,6 +20,12 @@ public class Player {
     private ArrayList<PlayerSubscriber> subscribers;
     private Context context;
 
+    /**
+     * Player constructor
+     * @param context where player is being spawned, needed for rendering sprite
+     * @param positionX initial X position of player
+     * @param positionY initial Y position of player
+     */
     public Player(Context context, double positionX, double positionY) {
         this.positionX = positionX;
         this.positionY = positionY;
@@ -27,6 +33,10 @@ public class Player {
 
     }
 
+    /**
+     * draw method to render sprite of player
+     * @param canvas where the player sprite is being drawn
+     */
     public void draw(Canvas canvas) {
         int sprite = PlayerData.getPlayer().getSpriteSelected();
         Bitmap spriteBitmap = BitmapFactory.decodeResource(context.getResources(), sprite, null);
@@ -38,12 +48,21 @@ public class Player {
         canvas.drawBitmap(scaledBitmap, (float) positionX, (float) positionY, null);
     }
 
+    /**
+     * movement method that calls specific movement type
+     * @param direction direction player is moving in
+     * @param collision collision object that helps check for collisions with walls and enemies
+     */
     public void move(char direction, Collision collision) {
         movement.walk(direction);
         movement.run(direction);
         notifySubscribers();
     }
 
+    /**
+     * subscribe method for entities that need to be updated with player's current position
+     * @param sub entity being subscribed
+     */
     public void subscribe(PlayerSubscriber sub) {
         if (subscribers == null) {
             subscribers = new ArrayList<>();
@@ -51,7 +70,9 @@ public class Player {
         subscribers.add(sub);
     }
 
-
+    /**
+     * updating all those subscribed to PlayerSubscriber with the player's current position
+     */
     protected void notifySubscribers() {
         for (PlayerSubscriber sub : subscribers) {
             sub.update(positionX, positionY);
@@ -90,7 +111,12 @@ public class Player {
     public void setAttackRadius(double attackRange) {
         this.attackRange = attackRange;
     }
-  
+
+    /**
+     * method with logic on attacking/killing enemy
+     * @param enemy enemy that is in range to be killed
+     * @return true if enemy is in the attackRange radius
+     */
     public boolean attack(Enemy enemy) {
         return ((((positionX + 100) - enemy.getPositionX()) * ((positionX + 100)
                 - enemy.getPositionX()) + (((positionY + 80) - enemy.getPositionY())
